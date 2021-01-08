cd3eef7e44efa27e2fb606f3927f02354e8b50ec
UnicornEatingPopcorn <unicorn.eating.popcorn@gmail.com>
2021-01-08 20:05:45 +0300

    create header

---
+++ src/components/App.js
@@ -0,0 +1,20 @@
+import React from "react";
+import { BrowserRouter, Route } from "react-router-dom";
+
+import MainPage from "./MainPage";
+import MyPage from "./MyPage";
+import Header from "./Header/Header";
+
+const App = () => {
+  return (
+    <BrowserRouter>
+      <div>
+        <Header />
+        <Route path="/" exact component={MainPage} />
+        <Route path="/mypage" component={MyPage} />
+      </div>
+    </BrowserRouter>
+  );
+};
+
+export default App;
---
+++ src/components/Header/Header.js
@@ -0,0 +1,25 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import "./Header.sass";
+import logo from "./../../assets/real-buy-logo.svg";
+
+const Header = () => {
+  return (
+    <div className="header">
+      <Link to="/">
+        <img src={logo} />
+      </Link>
+      <div className="header-list">
+        <Link>투자하기 </Link>
+        <Link>투자 인사이트 </Link>
+        <Link>회사소개 </Link>
+        <Link>고객지원</Link>
+      </div>
+      <div className="header-end">
+        <Link to="/mypage">홍길동님 마이페이지</Link>
+      </div>
+    </div>
+  );
+};
+
+export default Header;
