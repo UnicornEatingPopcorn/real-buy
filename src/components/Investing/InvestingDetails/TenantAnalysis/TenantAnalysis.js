a222e92a3a0e0af5701f6aa3537b6b2e02373cae
UnicornEatingPopcorn <unicorn.eating.popcorn@gmail.com>
2021-01-18 11:31:20 +0300

    create TenantAnalysis

--- src/components/Investing/InvestingDetails/TenantAnalysis/TenantAnalysis.js
+++ src/components/Investing/InvestingDetails/TenantAnalysis/TenantAnalysis.js
@@ -1,8 +1,34 @@
 import React from "react";
 import "./TenantAnalysis.sass";
+import arrowRight from "./../../../../assets/icons/AssetManagement/arrow-right.svg";
 
 const TenantAnalysis = () => {
-  return <h1> Tenant Analysis</h1>;
+  return (
+    <div className="tenant-analysis__container">
+      <p className="tenant-analysis__lessor">임대인 개요</p>
+
+      <div className="tenant-analysis__icons-box">
+        <span className="tenant-analysis__square">Amazon.com, Inc.</span>
+        <div className="tenant-analysis__method">
+          <img src={arrowRight} className="tenant-analysis__arrow" />
+          <span className="tenant-analysis__label">100% 소유</span>
+        </div>
+        <span className="tenant-analysis__square">
+          Amazon Europe Core S.a.r.l
+        </span>
+        <div className="tenant-analysis__method">
+          <img src={arrowRight} className="tenant-analysis__arrow" />
+          <span className="tenant-analysis__label">100% 소유</span>
+        </div>
+        <span className="tenant-analysis__square-last tenant-analysis__square">
+          <p>Amazon</p>
+          <p className="tenant-analysis__square-sm">
+            Logistik Mönchengladbach GmbH
+          </p>
+        </span>
+      </div>
+    </div>
+  );
 };
 
 export default TenantAnalysis;
