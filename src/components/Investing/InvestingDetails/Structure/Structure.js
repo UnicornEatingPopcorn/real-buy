52a738df90bd46886cfbaedd181d1beff3fd50c2
UnicornEatingPopcorn <unicorn.eating.popcorn@gmail.com>
2021-01-18 23:07:02 +0300

    implement Structure component

--- src/components/Investing/InvestingDetails/Structure/Structure.js
+++ src/components/Investing/InvestingDetails/Structure/Structure.js
@@ -1,8 +1,13 @@
 import React from "react";
 import "./Structure.sass";
+import structure from "./../../../../assets/icons/structure.png";
 
 const Structure = () => {
-  return <h1>Structure</h1>;
+  return (
+    <div className="structure__container">
+      <img src={structure} className="structure__image" />
+    </div>
+  );
 };
 
 export default Structure;
