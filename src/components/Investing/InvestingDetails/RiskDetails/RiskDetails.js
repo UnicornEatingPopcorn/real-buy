077f20e5ae131bddc95148f27d364855ac2bb793
UnicornEatingPopcorn <unicorn.eating.popcorn@gmail.com>
2021-01-20 05:33:07 +0300

    add RiskDetails

--- src/components/Investing/InvestingDetails/RiskDetails/RiskDetails.js
+++ src/components/Investing/InvestingDetails/RiskDetails/RiskDetails.js
@@ -1,8 +1,28 @@
 import React from "react";
 import "./RiskDetails.sass";
+import detailFirst from "./../../../../assets/icons/RiskDetails/risk-details-1.svg";
+import detailSecond from "./../../../../assets/icons/RiskDetails/risk-details-2.svg";
+import detailThird from "./../../../../assets/icons/RiskDetails/risk-details-3.svg";
 
 const RiskDetails = () => {
-  return <h1>Risk Details</h1>;
+  return (
+    <div className="risk-details">
+      <p className="risk-details__title">
+        자세한 정보를 보고 싶으시다면 아래의 문서를 다운 받아 주세요
+      </p>
+      <div className="row no-gutters">
+        <div className="col-6 col-lg-4 d-flex">
+          <img src={detailFirst} alt="detail" className="risk-details__pic" />
+        </div>
+        <div className="col-6 col-lg-4  d-flex">
+          <img src={detailSecond} alt="detail" className="risk-details__pic" />
+        </div>
+        <div className="col-6 col-lg-4 d-flex">
+          <img src={detailThird} alt="detail" className="risk-details__pic" />
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default RiskDetails;
