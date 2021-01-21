e7add76253d376f81bd3c11a1cc3e28215c95668
UnicornEatingPopcorn <unicorn.eating.popcorn@gmail.com>
2021-01-21 19:23:26 +0300

    add Login

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -2,7 +2,39 @@ import React from "react";
 import "./Login.sass";
 
 const Login = () => {
-  return <h1>Login</h1>;
+  return (
+    <div className="login">
+      <p className="login__title">로그인</p>
+      <div className="login__block">
+        <div className="login__box">
+          <div className="login__row">
+            <label className="login__input-title">이메일 계정 아이디</label>
+            <input
+              className="login__input"
+              type="text"
+              value="이메일 계정을 입력해 주세요"
+            />
+          </div>
+          <div className="login__row">
+            <label className="login__input-title">비밀번호</label>
+            <input
+              className="login__input-password login__input"
+              type="text"
+              value="대/소문자, 숫자, 기호 조합 8자 이상"
+            />
+          </div>
+          <div className="login__button-container">
+            <button className="login__button">로그인 하기</button>
+          </div>
+        </div>
+      </div>
+      <div className="login__buttons">
+        <button className="login__add-button">ID 찾기</button>
+        <button className="login__add-button">비밀번호 찾기</button>
+        <button className="login__add-button">회원 가입하기</button>
+      </div>
+    </div>
+  );
 };
 
 export default Login;
