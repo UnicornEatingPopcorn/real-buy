46d28ac346b36878ea3b76e83d676016c8ecd23c
UnicornEatingPopcorn <unicorn.eating.popcorn@gmail.com>
2021-01-21 19:52:17 +0300

    add SearchPassword

--- src/components/SearchPassword/SearchPassword.js
+++ src/components/SearchPassword/SearchPassword.js
@@ -4,39 +4,20 @@ import "./SearchPassword.sass";
 
 const SearchPassword = () => {
   return (
-    <div className="login">
-      <p className="login__title">로그인</p>
-      <div className="login__block">
-        <div className="login__box">
-          <div className="login__row">
-            <label className="login__input-title">이메일 계정 아이디</label>
-            <input
-              className="login__input"
-              type="text"
-              value="이메일 계정을 입력해 주세요"
-            />
-          </div>
-          <div className="login__row">
-            <label className="login__input-title">비밀번호</label>
-            <input
-              className="login__input-password login__input"
-              type="text"
-              value="대/소문자, 숫자, 기호 조합 8자 이상"
-            />
-          </div>
-          <div className="login__button-container">
-            <Link to="/mypage/dashboard">
-              <button className="login__button">로그인 하기</button>
-            </Link>
-          </div>
+    <div className="search-password">
+      <p className="search-password__title">비밀번호 찾기</p>
+      <div className="search-password__block">
+        <div className="search-password__box">
+          <p className="search-password__box-title">
+            회원가입 시 입력한 이메일 계정을 입력해주세요.
+            <br /> 비밀번호 인증 메일을 보내드립니다.
+          </p>
+          <input
+            className="search-password__input"
+            value="E-mail 주소를 입력해 주세요"
+          />
+          <button className="search-password__button">인증 메일 받기</button>
         </div>
-      </div>
-      <div className="login__buttons">
-        <button className="login__add-button">ID 찾기</button>
-        <button className="login__add-button">비밀번호 찾기</button>
-        <Link to="/register">
-          <button className="login__add-button">회원 가입하기</button>
-        </Link>
       </div>
     </div>
   );
