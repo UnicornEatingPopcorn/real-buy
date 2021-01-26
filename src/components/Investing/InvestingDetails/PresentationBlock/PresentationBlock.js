9690778e983ee1e36dac1502966775590db6ecc3
UnicornEatingPopcorn <unicorn.eating.popcorn@gmail.com>
2021-01-26 19:31:58 +0300

    mobile PresentationBlock

--- src/components/Investing/InvestingDetails/PresentationBlock/PresentationBlock.js
+++ src/components/Investing/InvestingDetails/PresentationBlock/PresentationBlock.js
@@ -6,35 +6,37 @@ const PresentationBlock = () => {
   return (
     <>
       <div className="details__presentation-block">
-        <div className="row no-gutters">
-          <div className="col col-sm-7 col-md-6 col-lg-6">
-            <span className="details__safety">
-              <p className="details__safety_text">투자 안전성</p>
-              <img
-                src={safetyIndex}
-                alt="safety index"
-                className="details__safety-index"
-              />
-            </span>
-            <p className="details__name">독일 아마존 물류 센터</p>
-            <span className="details__hashtags">#해외투자 #물류센터</span>
-          </div>
-          <div className="col col-md-5 offset-md-1 col-lg-4 offset-lg-2 details__terms-col">
-            <div className="details__terms">
-              <p className="details__terms_title">연수익률</p>
-              <span className="details__terms-flex">
-                <p className="details__terms_title-num">8.6</p>
-                <p className="details__terms_title-sign">%</p>
+        <div className="details__presentation-block-container">
+          <div className="row no-gutters">
+            <div className="col col-sm-9 col-md-6 col-lg-6">
+              <span className="details__safety">
+                <p className="details__safety_text">투자 안전성</p>
+                <img
+                  src={safetyIndex}
+                  alt="safety index"
+                  className="details__safety-index"
+                />
               </span>
+              <p className="details__name">독일 아마존 물류 센터</p>
+              <span className="details__hashtags">#해외투자 #물류센터</span>
             </div>
-            <div className="details__terms">
-              <p className="details__terms_title">투자만기</p>
-              <span className="details__terms-flex">
-                <p className="details__terms_title-num">12</p>
-                <p className="details__terms_title-subtitle">개월</p>
-              </span>
+            <div className="col col-sm-3 col-md-5 offset-md-1 col-lg-4 offset-lg-2 details__terms-col">
+              <div className="details__terms">
+                <p className="details__terms_title">연수익률</p>
+                <span className="details__terms-flex">
+                  <p className="details__terms_title-num">8.6</p>
+                  <p className="details__terms_title-sign">%</p>
+                </span>
+              </div>
+              <div className="details__terms">
+                <p className="details__terms_title">투자만기</p>
+                <span className="details__terms-flex">
+                  <p className="details__terms_title-num">12</p>
+                  <p className="details__terms_title-subtitle">개월</p>
+                </span>
+              </div>
+              <p className="details__schedule">2021.01.08 AM11:00 오픈예정</p>
             </div>
-            <p className="details__schedule">2021.01.08 AM11:00 오픈예정</p>
           </div>
         </div>
       </div>
