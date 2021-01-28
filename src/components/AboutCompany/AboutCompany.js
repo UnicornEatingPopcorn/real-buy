1e3414b9ebbe6c3fa0b962c37a1292cdf80023eb
UnicornEatingPopcorn <unicorn.eating.popcorn@gmail.com>
2021-01-28 11:17:52 +0300

    fix AboutCompany

--- src/components/AboutCompany/AboutCompany.js
+++ src/components/AboutCompany/AboutCompany.js
@@ -34,48 +34,53 @@ const AboutCompany = () => {
         </div>
       </div>
       <div className="be-rich">
-        <p className="be-rich__title">나도 부자가 되고싶다.</p>
-        <div className="be-rich__graphs">
-          <img
-            src={beRichFirstGraph}
-            alt="be rich graph"
-            className="be-rich__col"
-          />
-          <img
-            src={beRichSecondGraph}
-            alt="be rich graph"
-            className="be-rich__col"
-          />
-        </div>
-        <p className="be-rich__subtitle">
-          근로 소득만으로 아파트 가격 상승률을 따라가기 힘든 시기입니다. (근로
-          소득 &lt; 자본 소득) 저성장 기조가 장기화되면서 낮은 금융 소득으로
-          부자가 될 수 없습니다.
-        </p>
-        <div className="be-rich__inline">
-          <p className="be-rich__text">벌어지는 소득격차의 해결방안</p>
-          <img src={arrow} alt="arrow" className="be-rich__arrow" />
-          <p className="be-rich__text be-rich__text-underline">
-            상업용 부동산 투자 안전하고 높은 수익으로 부자의 꿈을 실현해보세요
+        <div className="be-rich__container">
+          <p className="be-rich__title">나도 부자가 되고싶다.</p>
+          <div className="be-rich__graphs">
+            <img
+              src={beRichFirstGraph}
+              alt="be rich graph"
+              className="be-rich__col"
+            />
+            <img
+              src={beRichSecondGraph}
+              alt="be rich graph"
+              className="be-rich__col"
+            />
+          </div>
+          <p className="be-rich__subtitle">
+            근로 소득만으로 아파트 가격 상승률을 따라가기 힘든 시기입니다. (근로
+            소득 &lt; 자본 소득) 저성장 기조가 장기화되면서 낮은 금융 소득으로
+            부자가 될 수 없습니다.
           </p>
+          <div className="be-rich__inline">
+            <p className="be-rich__text">벌어지는 소득격차의 해결방안</p>
+            <img src={arrow} alt="arrow" className="be-rich__arrow" />
+            <p className="be-rich__text be-rich__text-underline">
+              상업용 부동산 투자 안전하고 높은 수익으로 부자의 꿈을 실현해보세요
+            </p>
+          </div>
         </div>
       </div>
       <div className="goal">
+        <div className="goal__container">
         <div className="goal__pic-container">
           <img src={goal} alt="goal" className="goal__pic" />
         </div>
-        <img src={goalImg} alt="goal" className="goal__pic-big" />
+          <img src={goalImg} alt="goal" className="goal__pic-big" /></div>
       </div>
       <div className="mission">
+        <div className="mission__container">
         <div className="mission__pic-container">
-          <img src={mission} alt="mission" />
+          <img src={mission} alt="mission" className="mission__pic" />
         </div>
         <p className="mission__text">
           “세상 모든 데이터를 체계화하여” <br />
           투자를 쉽게 만들어 주는 플랫폼
-        </p>
+            </p></div>
       </div>
       <div className="platform">
+        <div className="platform__container">
         <p className="platform__title">AI기반 실물자산 투자 플랫폼</p>
         <p className="platform__subtitle">
           1. 글로벌 투자 Data를 체계적으로 분석하여 시장 변화와 성장성을 먼저
@@ -85,7 +90,8 @@ const AboutCompany = () => {
           <br />
           3. 투자자 성향에 맞춰 확보한 상품을 공급합니다.
         </p>
-        <img src={platform} alt="platform" className="platform__pic" />
+          <img src={platform} alt="platform" className="platform__pic" />
+            </div>
       </div>
       <Footer mode="dark" />
     </>
--- src/components/InvestingMethod/InvestingMethod.js
+++ src/components/InvestingMethod/InvestingMethod.js
@@ -17,147 +17,151 @@ import numberSix from "./../../assets/icons/InvestingMethod/06.svg";
 const InvestingMethod = () => {
   return (
     <div className="investing-method">
-      <img src={main} alt="main" className="investing-method__main-pic" />
+      <div className="investing-method__container">
+        <img src={main} alt="main" className="investing-method__main-pic" />
 
-      <div className="investing-method__pic-block">
-        <img
-          src={firstMethod}
-          alt="first method"
-          className="investing-method__left-icon"
-        />
-        <img
-          src={numberOne}
-          alt="number one"
-          className="investing-method__full-img"
-        />
-        <div className="investing-method__button-right-container">
-          <button className="investing-method__button-right investing-method__button">
-            REAL BUY 회원가입
-          </button>
+        <div className="investing-method__pic-block">
+          <img
+            src={firstMethod}
+            alt="first method"
+            className="investing-method__left-icon"
+          />
+          <img
+            src={numberOne}
+            alt="number one"
+            className="investing-method__full-img"
+          />
+          <div className="investing-method__button-right-container">
+            <button className="investing-method__button-right investing-method__button">
+              REAL BUY 회원가입
+            </button>
+          </div>
+          <div className="investing-method__container-large">
+            <button className="investing-method__button-right investing-method__button">
+              REAL BUY 회원가입
+            </button>
+          </div>
         </div>
-        <div className="investing-method__container-large">
-          <button className="investing-method__button-right investing-method__button">
-            REAL BUY 회원가입
-          </button>
-        </div>
-      </div>
 
-      <div className="investing-method__pic-block">
-        <img
-          src={numberTwo}
-          alt="number one"
-          className="investing-method__number-left investing-method__full-img"
-        />
-        <img
-          src={secondMethod}
-          alt="first method"
-          className="investing-method__right-icon"
-        />
-        <div className="investing-method__button-container">
-          <button className="investing-method__button-left investing-method__button">
-            가상계좌 개설 및 확인
-          </button>
+        <div className="investing-method__pic-block">
+          <img
+            src={numberTwo}
+            alt="number one"
+            className="investing-method__number-left investing-method__full-img"
+          />
+          <img
+            src={secondMethod}
+            alt="first method"
+            className="investing-method__right-icon"
+          />
+          <div className="investing-method__button-container">
+            <button className="investing-method__button-left investing-method__button">
+              가상계좌 개설 및 확인
+            </button>
+          </div>
+          <div className="investing-method__container-large">
+            <button className="investing-method__button-left investing-method__button">
+              가상계좌 개설 및 확인
+            </button>
+          </div>
         </div>
-        <div className="investing-method__container-large">
-          <button className="investing-method__button-left investing-method__button">
-            가상계좌 개설 및 확인
-          </button>
-        </div>
-      </div>
 
-      <div className="investing-method__pic-block">
-        <img
-          src={thirdMethod}
-          alt="first method"
-          className="investing-method__left-icon"
-        />
-        <img
-          src={numberThree}
-          alt="number one"
-          className="investing-method__full-img"
-        />
-        <div className="investing-method__button-right-container">
-          <button className="investing-method__button-right investing-method__button">
-            투자상품 확인
-          </button>
+        <div className="investing-method__pic-block">
+          <img
+            src={thirdMethod}
+            alt="first method"
+            className="investing-method__left-icon"
+          />
+          <img
+            src={numberThree}
+            alt="number one"
+            className="investing-method__full-img"
+          />
+          <div className="investing-method__button-right-container">
+            <button className="investing-method__button-right investing-method__button">
+              투자상품 확인
+            </button>
+          </div>
+          <div className="investing-method__container-large">
+            <button className="investing-method__button-right investing-method__button">
+              투자상품 확인
+            </button>
+          </div>
         </div>
-        <div className="investing-method__container-large">
-          <button className="investing-method__button-right investing-method__button">
-            투자상품 확인
-          </button>
+        <div className="investing-method__pic-block">
+          <img
+            src={numberFour}
+            alt="number one"
+            className="investing-method__number-left investing-method__full-img"
+          />
+          <img
+            src={fourthMethod}
+            alt="first method"
+            className="investing-method__right-icon"
+          />
+          <div className="investing-method__button-container">
+            <button className="investing-method__button-left investing-method__button">
+              투자하기
+            </button>
+          </div>
+          <div className="investing-method__container-large">
+            <button className="investing-method__button-left investing-method__button">
+              투자하기
+            </button>
+          </div>
         </div>
-      </div>
-      <div className="investing-method__pic-block">
-        <img
-          src={numberFour}
-          alt="number one"
-          className="investing-method__number-left investing-method__full-img"
-        />
-        <img
-          src={fourthMethod}
-          alt="first method"
-          className="investing-method__right-icon"
-        />
-        <div className="investing-method__button-container">
-          <button className="investing-method__button-left investing-method__button">
-            투자하기
-          </button>
+        <div className="investing-method__pic-block">
+          <img
+            src={fifthMethod}
+            alt="first method"
+            className="investing-method__left-icon"
+          />
+          <img
+            src={numberFive}
+            alt="number one"
+            className="investing-method__full-img"
+          />
+          <div className="investing-method__button-right-container">
+            <button className="investing-method__button-right investing-method__button">
+              투자 내역
+            </button>
+          </div>
+          <div className="investing-method__container-large">
+            <button className="investing-method__button-right investing-method__button">
+              투자 내역
+            </button>
+          </div>
         </div>
-        <div className="investing-method__container-large">
-          <button className="investing-method__button-left investing-method__button">
-            투자하기
-          </button>
-        </div>
-      </div>
-      <div className="investing-method__pic-block">
-        <img
-          src={fifthMethod}
-          alt="first method"
-          className="investing-method__left-icon"
-        />
-        <img
-          src={numberFive}
-          alt="number one"
-          className="investing-method__full-img"
-        />
-        <div className="investing-method__button-right-container">
-          <button className="investing-method__button-right investing-method__button">
-            투자 내역
-          </button>
-        </div>
-        <div className="investing-method__container-large">
-          <button className="investing-method__button-right investing-method__button">
-            투자 내역
-          </button>
-        </div>
-      </div>
-      <div className="investing-method__pic-block">
-        <img
-          src={numberSix}
-          alt="number one"
-          className="investing-method__number-left investing-method__full-img"
-        />
-        <img
-          src={sixthMethod}
-          alt="first method"
-          className="investing-method__right-icon"
-        />
-        <div className="investing-method__text-container">
-          <p className="investing-method__text-title">06. Sixth</p>
-          <p className="investing-method__text-subtitle">투자 현황 확인하기</p>
-          <p className="investing-method__text-sub">
-            투자 현황과 예상 수익금을 확인 하세요
-          </p>
-        </div>
-        <div className="investing-method__button-container">
-          <button className="investing-method__button-last investing-method__button">
-            마이페이지
-          </button>
-        </div>
-        <div className="investing-method__container-large">
-          <button className="investing-method__button-last investing-method__button">
-            마이페이지
-          </button>
+        <div className="investing-method__pic-block">
+          <img
+            src={numberSix}
+            alt="number one"
+            className="investing-method__number-left investing-method__full-img"
+          />
+          <img
+            src={sixthMethod}
+            alt="first method"
+            className="investing-method__right-icon"
+          />
+          <div className="investing-method__text-container">
+            <p className="investing-method__text-title">06. Sixth</p>
+            <p className="investing-method__text-subtitle">
+              투자 현황 확인하기
+            </p>
+            <p className="investing-method__text-sub">
+              투자 현황과 예상 수익금을 확인 하세요
+            </p>
+          </div>
+          <div className="investing-method__button-container">
+            <button className="investing-method__button-last investing-method__button">
+              마이페이지
+            </button>
+          </div>
+          <div className="investing-method__container-large">
+            <button className="investing-method__button-last investing-method__button">
+              마이페이지
+            </button>
+          </div>
         </div>
       </div>
     </div>
